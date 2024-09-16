7d23c2db1121063538bd772ce99cec294e60b4b0
herob <2284879033@qq.com>
2024-09-16 17:46:52 +0800

    copy link

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Obsidian Link Tools
 
 1. copy links
-2. quickly create wiki links
+2. match links
--- main.ts
+++ main.ts
@@ -1,62 +1,77 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, MarkdownView, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
-	mySetting: string;
+	linkFormat: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+	linkFormat: "${filePath}/${fileBasename}#${headingText}|${headingText}"
+};
+
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
+	copyLink(view: MarkdownView) {
+		const editor = view.editor
+		const cursor = editor.getCursor();
+		const lines = editor.getValue().split('\n');
 
+		// Find the nearest heading above the current cursor position
+		let headingText = null;
+		for (let i = cursor.line; i >= 0; i--) {
+			const line = lines[i].trim();
+			if (line.startsWith('#')) {
+				headingText = line.replace(/^#+\s*/, ''); // Remove '#' and spaces
+				break;
+			}
+		}
+
+		if (!headingText) {
+			new Notice('No heading found above the current cursor position.');
+			return;
+		}
+
+		// Get the current file path
+		// Check if the file exists
+		const file = view.file;
+		if (!file) {
+			new Notice('No file is associated with the current view.');
+			return;
+		}
+
+		// Get the current file's directory path and basename (without extension)
+		const filePath = file.path.substring(0, file.path.lastIndexOf('/'));
+		const fileBasename = file.basename
+		// Construct the Wiki Link (excluding .md extension)
+		const targetLink = `[[${this.settings.linkFormat
+			.replaceAll('${filePath}', filePath)
+			.replaceAll('${fileBasename}', fileBasename)
+			.replaceAll('${headingText}', headingText)}]]`;
+
+		// Copy to clipboard
+		navigator.clipboard.writeText(targetLink).then(() => {
+			new Notice('Wiki link copied to clipboard!');
+		}).catch(err => {
+			console.error('Failed to copy text: ', err);
+		});
+	}
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('hello?');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
 		// This adds a complex command that can check whether the current state of the app allows execution of the command
 		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
+			id: 'copy-link',
+			name: 'copy link',
 			checkCallback: (checking: boolean) => {
 				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
+				const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
+				if (activeView) {
 					// If checking is true, we're simply "checking" if the command can be run.
 					// If checking is false, then we want to actually perform the operation.
 					if (!checking) {
-						new SampleModal(this.app).open();
+						this.copyLink(activeView)
 					}
 
 					// This command will only show up in Command Palette when the check function returns true
@@ -65,14 +80,27 @@ export default class MyPlugin extends Plugin {
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.registerEvent(
+			this.app.workspace.on("editor-menu", (menu, editor, view) => {
+				if (view instanceof MarkdownView) {
+					menu.addItem((item) => {
+						item
+							.setTitle("Copy link")
+							.setIcon("external-link")
+							.onClick(async () => {
+								this.copyLink(view)
+							});
+					});
+				}
+				else {
+					new Notice('The current view is not a Markdown view.');
+				}
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+			})
+		);
+
+		// This adds a settings tab so the user can configure various aspects of the plugin
+		this.addSettingTab(new MySettingTab(this.app, this));
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
@@ -91,23 +119,7 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
+class MySettingTab extends PluginSettingTab {
 	plugin: MyPlugin;
 
 	constructor(app: App, plugin: MyPlugin) {
@@ -116,19 +128,22 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName('link format')
+			.setDesc('${filePath},${fileBasename},${headingText} are available')
+			.addText(text => {
+				text.setPlaceholder('Enter your format')
+					.setValue(this.plugin.settings.linkFormat || "${filePath}/${fileBasename}#${headingText}|${headingText}")
+					.onChange(async (value) => {
+						this.plugin.settings.linkFormat = value;
+						await this.plugin.saveSettings();
+
+					});
+				text.inputEl.style.width = '400px';
+			});
 	}
 }
